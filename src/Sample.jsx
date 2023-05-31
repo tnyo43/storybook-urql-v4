3acb98dcc1eaaf6daf3b2306edfe7778a03304ee
Tomoya Kashifuku <tnyo43@gmail.com>
2023-06-01 01:06:14 +0900

    remove unused component

--- src/Sample.jsx
+++
@@ -1,3 +0,0 @@
-export const Sample = () => {
-  return <div>sample</div>;
-};
--- src/Sample.stories.jsx
+++
@@ -1,8 +0,0 @@
-import { Sample } from './Sample';
-
-export default {
-  component: Sample,
-  tags: ['autodocs']
-};
-
-export const Default = {};
